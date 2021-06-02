db840b0f7e64749051ce57999b8113c3211bd29a
phillipphan <itsphillipphan@gmail.com>
2021-06-02 10:49:11 -0500

    added server side rendering for list of incomplete orders

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import userfavorite_list
+from .views import userfavorite_list, incompleteorder_list
 
 urlpatterns = [
     path('reports/userfavorites', userfavorite_list),
+    path('reports/incompleteorders', incompleteorder_list),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .connection import Connection
 from .users.favoritesbyuser import userfavorite_list
+from .orders.incompleteorders import incompleteorder_list
