454c372db5f026bfbcadb3b1ffb865fd8d3a12d1
phillipphan <itsphillipphan@gmail.com>
2021-06-02 13:37:44 -0500

    finished server side rendering for list of expensive products

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import userfavorite_list, incompleteorder_list, inexpensiveproduct_list
+from .views import userfavorite_list, incompleteorder_list, inexpensiveproduct_list, expensiveproduct_list
 
 urlpatterns = [
     path('reports/userfavorites', userfavorite_list),
     path('reports/incompleteorders', incompleteorder_list),
     path('reports/inexpensiveproducts', inexpensiveproduct_list),
+    path('reports/expensiveproducts', expensiveproduct_list),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .users.favoritesbyuser import userfavorite_list
 from .orders.incompleteorders import incompleteorder_list
 from .products.inexpensiveproducts import inexpensiveproduct_list
+from .products.expensiveproducts import expensiveproduct_list
