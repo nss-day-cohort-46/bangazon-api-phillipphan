193b1d25126a2f3b139105843c4019d41f5a248e
phillipphan <itsphillipphan@gmail.com>
2021-06-03 13:21:14 -0500

    changed query name

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -250,7 +250,7 @@ class Products(ViewSet):
         order = self.request.query_params.get('order_by', None)
         direction = self.request.query_params.get('direction', None)
         number_sold = self.request.query_params.get('number_sold', None)
-        price = self.request.query_params.get('price', None)
+        price = self.request.query_params.get('min_price', None)
 
         if order is not None:
             order_filter = order
